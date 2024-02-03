63cc2fa5dc0296847e26a0a40a2480e8e3a4af0e
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-02-03 17:16:43 +0200

    add code

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import random
+
+
+def game():
+    num = random.randint(0, 1)
+    if num == 1:
+        print("--- ОРЕЛ ---")
+    elif num == 0:
+        print("--- РЕШКА ---")
+    else:
+        print("--- СТАЛАСЬ ПОМИЛКА ---")
+
 
 if __name__ == "__main__":
-    print("Heloo! It's work!")
+    # game()
+    num = 0
+    while True:
+        num += 1
+        print(f"SPAM -- {num}")
